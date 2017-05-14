2253f3df231f97d4a9eaf2bb354d4f310bb74f86
Chris Rodgers <xrodgers@gmail.com>
2017-05-13 22:11:10 -0400

    adding user and review models

--- jtalk/admin.py
+++ jtalk/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import User, Review
 
 # Register your models here.
+
+
+
+admin.site.register(User)
+admin.site.register(Review)
--- jtalk/models.py
+++ jtalk/models.py
@@ -3,3 +3,14 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+
+
+class User(models.Model):
+    username = models.CharField(max_length=20, unique=True)
+    
+    def __str__(self):
+        return self.username
+
+class Review(models.Model):
+    author = models.ForeignKey(User)
+    score = models.IntegerField(null=True, blank=True)
